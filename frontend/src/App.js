89637765e56e2c898288b6a6a903bd6b74a75e27
Edgar <harove@gmail.com>
2022-10-13 14:00:37 -0300

    front and back working toghether

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path={`product/:id`} element={<ProductScreen />} />
+            <Route path="/cart" element={<HomeScreen />} />
+            <Route path="/login" element={<HomeScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
